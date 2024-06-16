fe548194b038bb28bcef9fa02a4c5aef5b96d5a3
youngconnorr <134354392+youngconnorr@users.noreply.github.com>
2024-06-16 14:00:58 -0600

    refactored landing page into section components

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -8,7 +8,6 @@ const Landing = () => {
 
   const isLanding = location.pathname === "/";
 
-  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
   if (isLanding) {
     window.addEventListener("scroll", function () {
       const navbar = document.querySelector(".navbar-absolute");
