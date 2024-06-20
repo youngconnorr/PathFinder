d072c02db46b669cc863ffd24c5da6964b0d6ac2
youngconnorr <134354392+youngconnorr@users.noreply.github.com>
2024-06-19 18:14:13 -0600

    added about page

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,31 +1,71 @@
 import "./About.module.css";
+import { useEffect } from "react";
 
 const About = () => {
-  return (
-    <section className="light-green grades-section">
-      <div className="school-section">
-        <h1 className="hidden about">About the developer:</h1>
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        // console.log(entry);
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          // console.log(entry + "added to show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    });
 
-        <section className="school-part-of-section">
-          <div className="description-section hidden">
-            <h2>Hi I&#39;m Connor!</h2>
-            <p>I&#39;m a software engineer from Calgary, Alberta.</p>
-            <p>
-              I’m a third year student at the University of British Columbia
-              studying computer science. Looking for software engineer
-              internships!
-            </p>
-            <p>
-              I enjoy connectioning with people, exploring new technology, and
-              developing cool projects with a particular interest in fullstack
-              development. Outside of tech - you can find me competing in sports
-              such as volleyball and bouldering, hiking up mountains with
-              friends, or just listening to good music 😄
-            </p>
-          </div>
-        </section>
-      </div>
-    </section>
+    const hiddenElements = document.querySelectorAll(".hidden, .img-hidden");
+    hiddenElements.forEach((el) => observer.observe(el));
+  }, []);
+  return (
+    <>
+      <section className="grades-section">
+        <div className="school-section">
+          <h1 className="hidden about">About the website:</h1>
+
+          <section className="school-part-of-section">
+            <div className="description-section hidden">
+              <h2 className="hidden spacer">About PathFinder:</h2>
+              <h3 className="hidden spacer">The Mission:</h3>
+              <p className="hidden spacer">
+                At PathFinder, we believe that travel should be an exciting,
+                stress-free experience. Our mission is to revolutionize the way
+                people plan their journeys by leveraging the power of artificial
+                intelligence to create personalized, seamless, and unforgettable
+                travel experiences.
+              </p>
+            </div>
+          </section>
+        </div>
+      </section>
+      <section className="me-section">
+        <div className="school-section">
+          <h1 className="hidden about ">About the developer:</h1>
+
+          <section className="school-part-of-section">
+            <div className="description-section hidden">
+              <h2 className="hidden spacer">Hi I&#39;m Connor!</h2>
+              <p className="hidden spacer">
+                I&#39;m a software engineer from Calgary, Alberta.
+              </p>
+              <p className="hidden spacer">
+                I’m a third year student at the University of British Columbia
+                studying computer science. Looking for software engineer
+                internships!
+              </p>
+              <p className="hidden spacer">
+                I enjoy connectioning with people, exploring new technology, and
+                developing cool projects with a particular interest in fullstack
+                development. Outside of tech - you can find me competing in
+                sports such as volleyball and bouldering, hiking up mountains
+                with friends, or just listening to good music 😄
+              </p>
+            </div>
+          </section>
+        </div>
+      </section>
+    </>
   );
 };
 
