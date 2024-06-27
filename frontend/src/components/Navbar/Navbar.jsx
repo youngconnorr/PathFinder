2ebd1da6297b6dad5aef5810824e982622c8a2c3
youngconnorr <134354392+youngconnorr@users.noreply.github.com>
2024-06-27 14:06:11 -0600

    made responsive navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,32 @@
 import { Link } from "react-router-dom";
-// import AxiosInstance from "./tools/AxiosInstance";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const token = localStorage.getItem("Token");
-  // const navigate = useNavigate();
   const location = useLocation();
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const transparentNav = ["/", "/login", "/register", "/profile", "/generate"];
   const isNotLanding = !transparentNav.includes(location.pathname);
   const isProfile = location.pathname === "/profile";
 
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
   return (
-    <>
+    <div
+      className={`
+      ${isNotLanding ? "navbar-not-landing" : "navbar-absolute"}
+      ${isProfile ? "black" : ""}`}
+    >
       <div
         className={`
         ${isNotLanding ? "navbar-not-landing" : "navbar-absolute"}
-        ${isProfile ? "black" : ""}`}
+        ${isProfile ? "black" : ""} not-collapsed`}
       >
         {token ? (
           <>
@@ -57,51 +67,59 @@ const Navbar = () => {
           </div>
         )}
       </div>
-      <div
+      <section
         className={`
         ${isNotLanding ? "navbar-not-landing" : "navbar-absolute"}
         ${isProfile ? "black" : ""} collapsed`}
       >
-        {token ? (
+        <div>
+          <button>
+            <Link to="/" className="navbar-black">
+              PathFinder
+            </Link>
+          </button>
+        </div>
+        {drawerOpen ? (
           <>
-            <div>
-              <button>
-                <Link to="/">PathFinder</Link>
-              </button>
-            </div>
-            <div>
-              <button>
-                <Link to="/generate">Generate</Link>
-              </button>
-              <button>
-                <Link to="/about">About Us</Link>
-              </button>
-              <button>
-                <Link to="/profile">Profile</Link>
-              </button>
-            </div>
+            {token ? (
+              <>
+                <button>
+                  <Link to="/generate" className="navbar-black">
+                    Generate
+                  </Link>
+                </button>
+                <button>
+                  <Link to="/about" className="navbar-black">
+                    About Us
+                  </Link>
+                </button>
+                <button>
+                  <Link to="/profile" className="navbar-black">
+                    Profile
+                  </Link>
+                </button>
+              </>
+            ) : (
+              <>
+                <button>
+                  <Link to="/about" className="navbar-black">
+                    About Us
+                  </Link>
+                </button>
+                <button>
+                  <Link to="/login" className="navbar-black">
+                    Log in
+                  </Link>
+                </button>
+              </>
+            )}
           </>
-        ) : (
-          <div className="nav-no-account-buttons">
-            <div>
-              <button>
-                <Link to="/">
-                  <b>PathFinder</b>
-                </Link>
-              </button>
-            </div>
-            <div>
-              <button>
-                <Link to="/about">About Us</Link>
-              </button>
-              <button>
-                <Link to="/login">Log in</Link>
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
-    </>
+        ) : null}
+        <button className="drawer-toggle navbar-black" onClick={toggleDrawer}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      </section>
+    </div>
   );
 };
 
